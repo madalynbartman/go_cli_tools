e0894da2996510f760174a4de12b8186762669a1
Madalyn <madalynbartman@gmail.com>
2025-02-01 15:59:54 -0700

    word count

---
+++ wc/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	// Calling the count function to count the number of words received from stdin and printing it
+	fmt.Println(count(os.Stdin))
+}
+
+func count(r io.Reader) int {
+	// A scanner is used to read text from a Reader (such as files)
+	scanner := bufio.NewScanner(r)
+
+	//Define the scanner split type to words (default is split by lines)
+	scanner.Split(bufio.ScanWords)
+
+	//Defining a counter
+	wc := 0
+
+	// For every word scanned, increment the counter
+	for scanner.Scan() {
+		wc++
+	}
+
+	//Return the total
+	return wc
+}
