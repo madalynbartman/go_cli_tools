ac99cd07383563c20da141a9bdada477b0b20aac
Madalyn <madalynbartman@gmail.com>
2025-02-01 17:03:51 -0700

    Add line count to the word count tool

--- wc/main.go
+++ wc/main.go
@@ -2,27 +2,34 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	// Calling the count function to count the number of words received from stdin and printing it
-	fmt.Println(count(os.Stdin))
+	// Defining a boolean flag -l to count lines instead of words
+	lines := flag.Bool("l", false, "Count lines")
+	// Parsingthe flags provided by the user
+	flag.Parse()
+	// Calling the count function to count the number of words (or lines) received from stdin and printing it
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
-	//Define the scanner split type to words (default is split by lines)
-	scanner.Split(bufio.ScanWords)
+	//If the count lines flag is not setm count words so we define the scanner split type to words (default is split by lines)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
 
 	//Defining a counter
 	wc := 0
 
-	// For every word scanned, increment the counter
+	// For every word or line scanned, increment the counter
 	for scanner.Scan() {
 		wc++
 	}
